783877328a9d287e882a254fce4f62ab3b85aa90
Ixsh <Daniel.Lee.Montoya@gmail.com>
2020-12-05 08:53:06 -0500

    Create solution1.py

---
+++ 2020/day2/solution1.py
@@ -0,0 +1,17 @@
+with open('C:/Users/durha/Desktop/input.txt', 'r', encoding='utf-8') as f:
+  arr = f.read().splitlines()
+
+f.close()
+
+def count_valid_passwords(passwords):
+    count = 0
+    for password in passwords:
+        password  = password.split(' ')
+        if len(password) == 3:
+            lower,upper = map(int, password[0].split('-'))
+            char = password[1].split(':')
+            if lower <= password[2].count(char[0]) <= upper:
+                count += 1
+    return count
+
+print(count_valid_passwords(arr))
